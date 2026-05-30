a99822c6dc2ae095cf41baca51d2dcdb0b4e3ba3
Devansh jain <devanshjaincampaign@gmail.com>
2026-05-30 14:17:49 +0530

    Time: 0 ms (100%), Space: 7.7 MB (88.12%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -1,17 +1,12 @@
 class Solution {
 public:
     int fib(int n) {
-        if(n<0){
-            return -1;
+        int term1=0,term2=1,nextterm=0;
+        for(int i=0;i<n;i++){
+            nextterm=term1+term2;
+            term1=term2;
+            term2=nextterm;
         }
-        else if(n==0){
-            return 0;
-        }
-        else if(n==1){
-            return 1;
-        }
-        else{
-            return fib(n-1)+fib(n-2);
-        }
+        return term1;
     }
 };
