ea174c4d981bb20cec2cf5d6b9feb0abc2096595
Devansh jain <devanshjaincampaign@gmail.com>
2026-06-02 10:30:36 +0530

    Time: 0 ms (100%), Space: 91.8 MB (59.92%) - LeetHub

---
+++ LeetCode/Easy/3633-earliest-finish-time-for-land-and-water-rides-i/3633-earliest-finish-time-for-land-and-water-rides-i.cpp
@@ -0,0 +1,38 @@
+#include <vector>
+#include <algorithm>
+#include <climits>
+
+using namespace std;
+
+class Solution {
+public:
+    int earliestFinishTime(vector<int>& landStartTime, vector<int>& landDuration, vector<int>& waterStartTime, vector<int>& waterDuration) {
+        
+        int minLandFinish = INT_MAX;
+        int minWaterFinish = INT_MAX;
+        
+        for(int i = 0; i < landStartTime.size(); i++){
+            int currentLandFinish = landStartTime[i] + landDuration[i];
+            minLandFinish = min(minLandFinish, currentLandFinish);
+        }
+        
+        for(int i = 0; i < waterStartTime.size(); i++){
+            int currentWaterFinish = waterStartTime[i] + waterDuration[i];
+            minWaterFinish = min(minWaterFinish, currentWaterFinish);
+        }
+
+        int landFirstTotal = INT_MAX;
+        for(int i = 0; i < waterStartTime.size(); i++) {
+            int actualWaterStart = max(minLandFinish, waterStartTime[i]);
+            landFirstTotal = min(landFirstTotal, actualWaterStart + waterDuration[i]);
+        }
+
+        int waterFirstTotal = INT_MAX;
+        for(int i = 0; i < landStartTime.size(); i++) {
+            int actualLandStart = max(minWaterFinish, landStartTime[i]);
+            waterFirstTotal = min(waterFirstTotal, actualLandStart + landDuration[i]);
+        }
+
+        return min(landFirstTotal, waterFirstTotal);
+    }
+};
